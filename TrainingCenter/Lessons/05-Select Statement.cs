cf1c05932bb421f4579eac2f49400a603d651134
bilmoh287 <142092794+bilmoh287@users.noreply.github.com>
2026-07-09 15:44:45 +0300

    Select only what matters

---
+++ TrainingCenter/Lessons/05-Select Statement.cs
@@ -0,0 +1,74 @@
+﻿//using Microsoft.EntityFrameworkCore;
+//using Microsoft.Extensions.Configuration;
+//using Microsoft.Extensions.Logging;
+//using TrainingCenter.Data;
+//using TrainingCenter.Entities;
+
+
+//// Configuration setup
+//IConfiguration configuration = new ConfigurationBuilder()
+//    .SetBasePath(Directory.GetCurrentDirectory())
+//    .AddJsonFile("appsettings.json", optional: false)
+//    .Build();
+
+//string? connectionString = configuration.GetConnectionString("DefaultConnection");
+
+//if (string.IsNullOrWhiteSpace(connectionString))
+//{
+//    Console.WriteLine("Connection string not found.");
+//    return;
+//}
+
+//// Create options with logging
+//var options =
+//    new DbContextOptionsBuilder<AppDbContext>()
+//        .UseSqlServer(connectionString)
+//        .LogTo(Console.WriteLine, LogLevel.Information)
+//        .EnableSensitiveDataLogging()
+//        .Options;
+
+
+//using var context = new AppDbContext(options);
+
+//Console.WriteLine("Connected successfully.");
+//Console.WriteLine();
+
+
+//// Call examples
+//GetStudentNames(context);
+
+
+//static void GetStudentNames(AppDbContext context)
+//{
+//    var query = context.Students
+//        .Select(s => new { s.FirstName, s.LastName });
+
+
+//    // Preview SQL before execution
+//    PreviewSQLUsingToQueryString(query.ToQueryString());
+
+//    // Execute query
+//    var students = query.ToList();
+
+//    // Print resultsa
+//    Console.WriteLine("\n\nStudent Names:");
+//    Console.WriteLine("--------------");
+
+//    foreach (var student in students)
+//    {
+//        Console.WriteLine($"{student.FirstName} {student.LastName}");
+//    }
+
+//    Console.WriteLine();
+//    Console.WriteLine($"\nTotal Students: {students.Count}");
+//    Console.WriteLine();
+//}
+
+//static void PreviewSQLUsingToQueryString(string SQLString)
+//{
+//    Console.WriteLine("\nPreview SQL using ToQueryString():");
+//    Console.WriteLine("----------------------------------");
+//    Console.WriteLine(SQLString);
+//    Console.WriteLine();
+
+//}
--- TrainingCenter/Program.cs
+++ TrainingCenter/Program.cs
@@ -35,73 +35,35 @@ Console.WriteLine();
 
 
 // Call examples
-FindStudentByIdUSingFind(context);
-FindStudentByIdUSingFirstOrDefault(context);
+GetStudentNames(context);
 
 
-static void FindStudentByIdUSingFind(AppDbContext context)
+static void GetStudentNames(AppDbContext context)
 {
-
-    var Student = context.Students.Find(1);
-    var Studen2 = context.Students.Find(1);
-
-
-    // Does not upport ToQueryString
-    //PreviewSQLUsingToQueryString(Student.ToQueryString());
-
-    PrintStudent(Student);
-    PrintStudent(Studen2);
-
-}
-
-/// <summary>
-/// FirstOrDefault() returns the first matching row,
-/// or null if no row exists.
-/// </summary>
-static void FindStudentByIdUSingFirstOrDefault(AppDbContext context)
-{
-    Console.WriteLine("\nExample 2 - FirstOrDefault()");
-    Console.WriteLine("----------------------------");
-
     var query = context.Students
-        .Where(s => s.StudentId == 1);
+        .Select(s => new { s.FirstName, s.LastName });
 
-    // Preview query shape
+
+    // Preview SQL before execution
     PreviewSQLUsingToQueryString(query.ToQueryString());
 
     // Execute query
-    // Runtime logging will show the actual executed SQL.
-    var student = query.FirstOrDefault();
+    var students = query.ToList();
 
-    if (student == null)
-    {
-        Console.WriteLine("\nNo student found.");
-    }
-    else
-    {
-        Console.WriteLine("\nStudent Found:");
-        Console.WriteLine($"{student.StudentId} - {student.FirstName} {student.LastName}");
-    }
-}
+    // Print resultsa
+    Console.WriteLine("\n\nStudent Names:");
+    Console.WriteLine("--------------");
 
-/// <summary>
-/// Prints student information in readable format.
-/// </summary>
-static void PrintStudent(dynamic? student)
-{
-    if (student != null)
+    foreach (var student in students)
     {
-        Console.WriteLine("\n\nStudent Found:");
-        Console.WriteLine(
-            $"{student.StudentId} - {student.FirstName} {student.LastName}");
-    }
-    else
-    {
-        Console.WriteLine("Student not found.");
+        Console.WriteLine($"{student.FirstName} {student.LastName}");
     }
 
     Console.WriteLine();
+    Console.WriteLine($"\nTotal Students: {students.Count}");
+    Console.WriteLine();
 }
+
 static void PreviewSQLUsingToQueryString(string SQLString)
 {
     Console.WriteLine("\nPreview SQL using ToQueryString():");
